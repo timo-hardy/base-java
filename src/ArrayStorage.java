f3de3bd8009552231fe768ab0522ed9abf8ac5c8
timos-java <timos.gooogle@gmail.com>
2019-11-03 20:04:36 +0300

    Realization methods in class ArrayStorage

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
@@ -5,26 +7,34 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        Arrays.fill(storage, null);
     }
 
     void save(Resume r) {
+        Arrays.fill(storage, r);
     }
 
     Resume get(String uuid) {
+            if (storage[storage.length - 1].uuid == uuid) {
+                return storage[storage.length - 1];
+            }
         return null;
     }
 
     void delete(String uuid) {
+        if (storage[storage.length - 1].uuid == uuid) {
+            Arrays.fill(storage, null);
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, storage.length);
     }
 
     int size() {
-        return 0;
+        return storage.length;
     }
 }
