af9b68f141abd9fdf47a064072555a7fcf307290
timos-java <timos.gooogle@gmail.com>
2019-12-08 15:53:19 +0300

    Implementation Abstract class

--- src/com/urise/webapp/storage/AbstractArrayStorage.java
+++ src/com/urise/webapp/storage/AbstractArrayStorage.java
@@ -2,15 +2,64 @@ package com.urise.webapp.storage;
 
 import com.urise.webapp.model.Resume;
 
+import java.util.Arrays;
+
+/**
+ * Array based storage for Resumes
+ */
 public abstract class AbstractArrayStorage implements Storage {
     private static final int STORAGE_LIMIT = 10000;
     protected Resume[] storage = new Resume[STORAGE_LIMIT];
     protected int size;
 
     @Override
+    public void clear() {
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
+    }
+
+    @Override
+    public void update(Resume r) {
+        int index = getIndex(r.getUuid());
+        if (index == -1) {
+            System.out.println("Resume with such" + r.getUuid() + " not exist");
+        } else {
+            storage[index] = r;
+        }
+    }
+
+    @Override
+    public void save(Resume resume) {
+        if (getIndex(resume.getUuid()) != -1) {
+            System.out.println("Resume with such" + resume.getUuid() + " already exist");
+        } else if (size == STORAGE_LIMIT) {
+            System.out.println("The resume array is overflow");
+        } else {
+            storage[size] = resume;
+            size++;
+        }
+    }
+
+    @Override
+    public void delete(String uuid) {
+        int index = getIndex(uuid);
+        if (index == -1) {
+            System.out.println("Resume with such " + uuid + " not exist");
+        } else {
+            storage[index] = storage[size - 1];
+            storage[size - 1] = null;
+            size--;
+        }
+    }
+
+    @Override
     public int size() {
         return size;
     }
+
+    /**
+     * @return array, contains only Resumes in storage (without null)
+     */
 
     @Override
     public Resume get(String uuid) {
@@ -20,6 +69,11 @@ public abstract class AbstractArrayStorage implements Storage {
             return null;
         }
         return storage[index];
+    }
+
+    @Override
+    public Resume[] getAll() {
+        return Arrays.copyOfRange(storage, 0, size);
     }
 
     protected abstract int getIndex(String uuid);
--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -2,63 +2,10 @@ package com.urise.webapp.storage;
 
 import com.urise.webapp.model.Resume;
 
-import java.util.Arrays;
-
-/**
- * Array based storage for Resumes
- */
 public class ArrayStorage extends AbstractArrayStorage {
     private static final int STORAGE_LIMIT = 10000;
     private int size;
     private Resume[] storage = new Resume[STORAGE_LIMIT];
-
-    @Override
-    public void clear() {
-        Arrays.fill(storage, 0, size, null);
-        size = 0;
-    }
-
-    @Override
-    public void update(Resume r) {
-        int index = getIndex(r.getUuid());
-        if (index == -1) {
-            System.out.println("Resume with such" + r.getUuid() + " not exist");
-        } else {
-            storage[index] = r;
-        }
-    }
-
-    @Override
-    public void save(Resume resume) {
-        if (getIndex(resume.getUuid()) != -1) {
-            System.out.println("Resume with such" + resume.getUuid() + " already exist");
-        } else if (size == STORAGE_LIMIT) {
-            System.out.println("The resume array is overflow");
-        } else {
-            storage[size] = resume;
-            size++;
-        }
-    }
-
-    @Override
-    public void delete(String uuid) {
-        int index = getIndex(uuid);
-        if (index == -1) {
-            System.out.println("Resume with such " + uuid + " not exist");
-        } else {
-            storage[index] = storage[size - 1];
-            storage[size - 1] = null;
-            size--;
-        }
-    }
-
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
-    @Override
-    public Resume[] getAll() {
-        return Arrays.copyOfRange(storage, 0, size);
-    }
 
     protected int getIndex(String uuid) {
         for (int i = 0; i < size; i++) {
