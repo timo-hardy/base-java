2556c9b54680fdd4286a774abe12a0383b36bc15
timos-java <timos.gooogle@gmail.com>
2020-01-25 16:19:46 +0300

    Implement methods with Storage

--- src/com/urise/webapp/ListStorage.java
+++ src/com/urise/webapp/ListStorage.java
@@ -1,4 +1,42 @@
 package com.urise.webapp;
 
-public class ListStorage {
+import com.urise.webapp.model.Resume;
+import com.urise.webapp.storage.Storage;
+
+public class ListStorage implements Storage {
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public void update(Resume r) {
+
+    }
+
+    @Override
+    public void save(Resume resume) {
+
+    }
+
+    @Override
+    public Resume get(String uuid) {
+        return null;
+    }
+
+    @Override
+    public void delete(String uuid) {
+
+    }
+
+    @Override
+    public Resume[] getAll() {
+        return new Resume[0];
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
 }
